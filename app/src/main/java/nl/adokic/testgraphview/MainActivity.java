f1422f999d0333dffafbb2864d092e2167b09611
Aleksandro <aco-dokic@live.nl>
2016-09-01 13:07:53 +0200

    grafiek en checkboxes

--- app/src/main/java/nl/adokic/testgraphview/MainActivity.java
+++ app/src/main/java/nl/adokic/testgraphview/MainActivity.java
@@ -1,8 +1,11 @@
 package nl.adokic.testgraphview;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jjoe64.graphview.DefaultLabelFormatter;
@@ -20,6 +23,7 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     LineGraphSeries<DataPoint> series, series2;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +41,14 @@ public class MainActivity extends AppCompatActivity {
         series2.setThickness(15);
         series2.setDataPointsRadius(10);
 
+        textView = (TextView) findViewById(R.id.textView);
+
 
         double x, y[], y1[];
         x = 0;
 
-        y = new double[] {2, 3, 2, 4, 3, 5};
-        y1 = new double[] {1, 2, 2, 3, 3, 4};
+        y = new double[] {5, 3, 4, 2, 3, 2};
+        y1 = new double[] {4, 3, 3, 2, 2, 1};
 
         for (int i = 0; i < 6; i++){
             x = x + 1;
@@ -53,36 +59,49 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        graphView.addSeries(series);
         graphView.addSeries(series2);
+        graphView.addSeries(series);
+
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                textView.setText("Clicked: "+dataPoint);
+            }
+        });
+
+        graphView.getViewport().setScrollable(true);
 
 
 
         StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView);
         staticLabelsFormatter.setVerticalLabels(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"});
-        staticLabelsFormatter.setHorizontalLabels(new String[] {"P-6", "P-5", "P-4", "P-3", "P-2", "P-1", "P-0"});
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"P-5", "P-4", "P-3", "P-3", "P-2", "P-0"});
         graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
 
 // set manual X bounds
         graphView.getViewport().setXAxisBoundsManual(true);
-        graphView.getViewport().setMaxX(7);
+//        graphView.getViewport().setMaxX(7);
+//        graphView.getViewport().setMinX(0);
 
 // set manual Y bounds
         graphView.getViewport().setYAxisBoundsManual(true);
-        graphView.getViewport().setMinY(0.0);
+        graphView.getViewport().setMinY(0);
         graphView.getViewport().setMaxY(9);
 
-        series.setTitle("foo");
-        series2.setTitle("bar");
+        series2.setTitle("Required");
+        series.setTitle("Scored");
         graphView.getLegendRenderer().setVisible(true);
         graphView.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        graphView.getLegendRenderer().setTextColor(Color.WHITE);
 
-
-        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTap(Series series, DataPointInterface dataPoint) {
-                Toast.makeText(getApplicationContext(), "Series: On Data Point clicked: "+dataPoint, Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
             }
         });
+
     }
 }
